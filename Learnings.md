e46096e88a1ad595045d44f24ba4f00e3fd57b5f
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2024-03-17 01:55:17 +0530

    .

---
+++ Learnings.md
@@ -0,0 +1,12 @@
+#### GraphQL
+
+#### React query
+
+```yarn add @tanstack/react-query```
+
+- We find the main entry point of our application and wrap it with ```queryClientProvider```, this will give react query the access to hold the cache, manage state and other features.
+
+- ```useQuery``` we use this whenever we want to fetch data. [GET]
+![alt text](image.png)
+
+- ```useMutation``` we use this for ( POST, PUT, DELETE requests) and manage their state.
--- twitter-client/pages/_app.tsx
+++ twitter-client/pages/_app.tsx
@@ -2,15 +2,24 @@ import "@/styles/globals.css";
 import { Inter, Quicksand } from "next/font/google";
 import type { AppProps } from "next/app";
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 const quickSand = Quicksand({ subsets: ["latin"] });
+
+const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
+      <QueryClientProvider client={queryClient}>
       <GoogleOAuthProvider clientId="851242117159-7qsf518qj22v3j87s2sjlbkjebrs3mou.apps.googleusercontent.com">
       <Component {...pageProps} />
+      <Toaster/>
+      <ReactQueryDevtools/>
       </GoogleOAuthProvider>
+      </QueryClientProvider>
     </div>
   );
 }
